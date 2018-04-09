cfce74c369585ea0c7ba6567b66bf425defedc77
leo mouraire <l.mouraire@outlook.fr>
2018-04-09 17:53:53 +0200

    add bootstrap and pdf

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :contents
+
+  root "application#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
